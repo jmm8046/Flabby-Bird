1816ea921b55d6cd8a83b26d7e8d2d97e3a3f28b
AkiraTheDev <48803571+AkiraTheDev@users.noreply.github.com>
2019-04-18 22:59:14 -0400

    Added a generator
    generator for the test fruit(coins) and obstacles

---
+++ Flabby Bird/Assets/Scripts/Generator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField] private GameObject fruitPrefab;
+    [SerializeField] private GameObject obsPrefab;
+
+    private List<GameObject> fruitList;
+    private List<GameObject> obsList;
+
+    private float fruitTimer = 5;
+    private float obsTimer = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fruitList = new List<GameObject>();
+        obsList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fruitTimer -= Time.deltaTime;
+        if(fruitTimer <= 0)
+        {
+            fruitTimer = 5;
+
+            GameObject clone = Instantiate(fruitPrefab, new Vector3(10, Random.Range(-2.5f, 2.5f), 0), Quaternion.identity);
+            fruitList.Add(clone);
+        }
+
+        obsTimer -= Time.deltaTime;
+        if (obsTimer <= 0)
+        {
+            obsTimer = 5;
+
+            GameObject clone = Instantiate(obsPrefab, new Vector3(10, -2.78f, 0), Quaternion.identity);
+            obsList.Add(clone);
+        }
+    }
+
+    public List<GameObject> getFruitList()
+    {
+        return fruitList;
+    }
+    
+    public List<GameObject> getObsList()
+    {
+        return obsList;
+    }
+}
--- Flabby Bird/Assets/Scripts/ScrollManager.cs
+++ Flabby Bird/Assets/Scripts/ScrollManager.cs
@@ -6,8 +6,13 @@ public class ScrollManager : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed;
 
+    [SerializeField] private Generator generator;
+
     [SerializeField] private GameObject ground;
     [SerializeField] private GameObject background;
+
+    private List<GameObject> fruitList;
+    private List<GameObject> obsList;
 
     private Vector3 groundStartPos;
     private Vector3 backStartPos;
@@ -15,6 +20,9 @@ public class ScrollManager : MonoBehaviour
     // Start is called before first frame update
     private void Start()
     {
+        fruitList = new List<GameObject>();
+        obsList = new List<GameObject>();
+
         groundStartPos = ground.transform.position;
         backStartPos = background.transform.position;
     }
@@ -22,12 +30,42 @@ public class ScrollManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        fruitList = generator.getFruitList();
+
+        foreach(GameObject fruit in fruitList.ToArray())
+        {
+            fruit.transform.Translate(Vector3.left * scrollSpeed * 0.05f);
+
+            Vector3 fruitPos = fruit.transform.position;
+
+            if(fruitPos.x < -10f)
+            {
+                fruitList.Remove(fruit);
+                Destroy(fruit);
+            }
+        }
+
+        obsList = generator.getObsList();
+
+        foreach(GameObject obs in obsList.ToArray())
+        {
+            obs.transform.Translate(Vector3.left * scrollSpeed * 0.05f);
+
+            Vector3 obsPos = obs.transform.position;
+
+            if(obsPos.x < -10f)
+            {
+                obsList.Remove(obs);
+                Destroy(obs);
+            }
+        }
+
         ground.transform.Translate(Vector3.left * scrollSpeed * 0.05f);
         background.transform.Translate(Vector3.left * scrollSpeed * 0.05f);
 
         Vector3 groundPos = ground.transform.position;
 
-        if (groundPos.x < -1)
+        if (groundPos.x < -1f)
         {
             ground.transform.SetPositionAndRotation(groundStartPos, Quaternion.identity);
         }
